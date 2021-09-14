fe0043853687096763e2a4c52f875d07f3e969e1
Opilane TTHK <opilane@tthk.ee>
2021-09-14 13:57:57 +0300

    Commit PersonalityTest

---
+++ GradingIf/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GradingIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mis hinde sa said?");
+            string UserNumber = Console.ReadLine();
+
+            if (UserNumber == "5")
+            {
+                Console.WriteLine("Suurepärane");
+            }
+            else if (UserNumber == "4")
+            {
+                Console.WriteLine("Väga hea");
+            }
+            else if (UserNumber == "3")
+            {
+                Console.WriteLine("Hea");
+            }
+            else if (UserNumber == "2")
+            {
+                Console.WriteLine("Rahuldav");
+            }
+            else if (UserNumber == "1")
+            {
+                Console.WriteLine("Puudulik");
+            }
+            else
+            {
+                Console.WriteLine("Pole hinnet");
+            }
+        }
+    }
+}
---
+++ GradingSwitch/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GradingSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mis hinde sa said?");
+            string UserNumber = Console.ReadLine();
+
+            switch (UserNumber)
+            {
+                case "5":
+                    Console.WriteLine("Suurepärane");
+                    break;
+                case "4":
+                    Console.WriteLine("Väga hea");
+                    break;
+                case "3":
+                    Console.WriteLine("Hea");
+                    break;
+                case "2":
+                    Console.WriteLine("Rahuldav");
+                    break;
+                case "1":
+                    Console.WriteLine("Puudulik");
+                    break;
+                default:
+                    Console.WriteLine("Pole hinnet");
+                    break;
+            }
+        }
+    }
+}
